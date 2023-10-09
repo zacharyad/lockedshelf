2aeba089462258aaf3d987a7d345d074647679ff
zacharyad <zacharyadcoding@gmail.com>
2023-10-09 09:40:17 -0400

    Added rerender to the handleResetPuzzle func

--- src/app/withbook/page.jsx
+++ src/app/withbook/page.jsx
@@ -80,6 +80,7 @@ function WithMain() {
   const handleResetPuzzles = () => {
     localStorage.removeItem('puzzle-data');
     console.log('DELETED');
+    rerender((prev) => !prev);
     window.location.reload();
   };
 
