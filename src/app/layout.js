39f0445c47417f438a99532c54ffdf5fd5ba7bb7
zacharyad <zacharyadcoding@gmail.com>
2023-10-11 10:06:41 -0400

    Styled the site to be lighter, added hero logo image with descriptor and button to go to the shelf section, added info nav link to go to info section (yet to be styled), added initials logo to top nav, and changed wording for the dynamic shelf header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Spacer from '@/components/spacer';
 const inter = Inter({ subsets: ['latin'] });
@@ -12,10 +13,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <body className={`${inter.className} `}>
-        <nav className="flex justify-around items-center h-32 shadow-xl shadow-slate-800">
-          <div className="m-auto text-4xl ml-12">
-            <Link href="/">Locked Shelf</Link>
+      <body className={`${inter.className} scroll-smooth`}>
+        <nav className="flex justify-around items-center h-32 shadow-xl shadow-slate-400">
+          <div className="m-auto text-2xl ml-12 flex justify-between items-center w-screen">
+            <Link href="/">
+              <Image
+                className="h-32 w-full p-4"
+                height={100}
+                width={100}
+                src="/assets/LOGO_in.png"
+              />
+            </Link>
+            <Link className="pr-8" href="#info">
+              Info
+            </Link>
           </div>
         </nav>
         <Spacer size={8} />
