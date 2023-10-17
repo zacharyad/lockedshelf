544d0acf7ca4c3cefd41f54eed35d1ea07a52687
zacharyad <zacharyadcoding@gmail.com>
2023-10-17 16:50:00 -0400

    Added the data to the faq variable and it is showing up on the faq component properly

--- src/data.js
+++ src/data.js
@@ -152,8 +152,39 @@ export const intialHolidayBookData = {
 };
 
 export const allBooks = [intialHolidayBookData];
-
+// \'
 export const faqs = [
-  { question: '1', answer: 'one' },
-  { question: '2', answer: 'two' },
+  {
+    question:
+      "How can I access the book's answer entry page, and what is the prerequisite for gaining access to this page?",
+    answer:
+      "You can access the book's answer entry page by using the special website link provided in the book you've bought. This page is only accessible to those who have purchased the book and used the link from the book. This ensures that only the book's rightful owners can join the interactive puzzle experience on the website.",
+  },
+  {
+    question:
+      "Where can I access the book's answer entry page on the Locked Shelf website, and how are subsequent books handled?",
+    answer:
+      "After you've used the link in the book to access the answer entry page, that book will be shown on the homepage for easy access back to the answer entry page. You can easily find your owned books' answer entry pages directly on the homepage of LockedShelf.com. Books you haven't bought will appear as links on the homepage, allowing you to conveniently buy them directly from the website",
+  },
+  {
+    question:
+      'How are my answers stored and managed on the browser to ensure data privacy?',
+    answer:
+      "Your answers are stored securely in your browser's memory, ensuring your data remains private and accessible only to you on your device.",
+  },
+  {
+    question: 'Will I be timed on how fast I solve a book?',
+    answer:
+      'The time taken to solve each individual puzzle is calculated from the moment you open the book\'s “Answer entry page” to when you solve it. The total "solved time" for the whole book is the sum of the times taken to solve each puzzle.',
+  },
+  {
+    question: 'Can I compete with friends and/or family?',
+    answer:
+      "You can compete with your friends based on the number of tries it takes to solve each puzzle and the total time taken to complete the entire book. Real-time competition might be possible by comparing each person's progress and results.",
+  },
+  {
+    question: 'Is there a way to get hints?',
+    answer:
+      "Yes, there's a \"hint toggle\" button on the book's answer entry page. It reveals additional clues for puzzles that you've attempted more than 5 times. If you find a puzzle too difficult, you can use the hints. We don't keep track of the hint button's use, so it's on the honor system if you're competing with friends or family.",
+  },
 ];
