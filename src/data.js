d7444a7ddf9d380e100ab784305ceb4f58338a05
zacharyad <zacharyadcoding@gmail.com>
2023-10-27 12:55:50 -0400

    Added comment flags to the data so that they do not get changed

--- src/data.js
+++ src/data.js
@@ -3,44 +3,44 @@ export const intialWITHBookData = {
   snapshot: 5,
   name: 'Whispers in the Hollow \r\n (Coming Oct. 2024)',
   bookImage: '/assets/test.jpg',
-  isSolved: false,
+  isSolved: false, // Do not change this ever
   href: '/withbook',
   timeEnded: undefined,
   timeStarted: new Date().toUTCString(),
   amazonWebAddress: 'https://www.amazon.com',
   puzzles: [
     {
-      id: 0,
+      id: 0, // Do not change this ever
       name: 'Spine',
       imageSrc: '/assets/test.jpg',
       imageAlt: 'image that has the letters 1P1U2Z1L1E',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['mr. van tassel', 'van tassel', 'mr van tassel'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'Use the numbers next to letters to give about of that letter found in final word. The letters sort of go in order.',
     },
     {
-      id: 1,
+      id: 1, // Do not change this ever
       name: 'MHK Map',
       imageSrc: '/assets/test.jpg',
       imageAlt: 'image of an outlined shape',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['1233'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'the places give you a number based on the map',
     },
     {
-      id: 2,
+      id: 2, // Do not change this ever
       name: 'Math',
       imageSrc: '/assets/test.jpg',
       imageAlt:
         'Images has text that has a triangle with addition sign followed by a 4',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['00000'],
-      timeSolved: new Date(),
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever,
+      tryCount: 0, // Do not change this ever
       hint: 'the places give you a number based on the map',
     },
   ],
@@ -51,7 +51,7 @@ export const intialHolidayBookData = {
   snapshot: 2,
   name: 'A Carol of Clues',
   bookImage: '/assets/acarolofclues.png',
-  isSolved: false,
+  isSolved: false, // Do not change this ever
   href: '/acarolofclues110123',
   imageAlt:
     'Image of a bearded Scrooge holding a key on the right side of the image and a door with wreaths all around it with people exiting the doow to a snowy scene',
@@ -60,142 +60,142 @@ export const intialHolidayBookData = {
   amazonWebAddress: 'https://www.amazon.com',
   puzzles: [
     {
-      id: 0,
+      id: 0, // Do not change this ever
       name: 'Birds',
       imageSrc: `/assets/acarolofclues/puzzleImage0.png`,
       imageAlt:
         'Long exposed of birds flying, showing you a line of birds streaking accross the sky in the image',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: [
         'may you always have time to enjoy the birds.',
         'may you always have time to enjoy the birds',
       ],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: "This trail may be seen using binaculars but i won't squak if you dont use BsThermometer. what does it say in quotes?",
     },
     {
-      id: 1,
+      id: 1, // Do not change this ever
       name: 'Binary',
       imageSrc: `/assets/acarolofclues/puzzleImage1.png`,
       imageAlt: "An image filled with 1's and 0's",
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['800'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'You may need to take note of the length of those lines.',
     },
     {
-      id: 2,
+      id: 2, // Do not change this ever
       name: 'MHK',
       imageSrc: `/assets/acarolofclues/puzzleImage2.png`,
       imageAlt: 'Image of the outline of Manhattan, Kansas',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['433722528637'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'Each place should be found in Manhattan... Should be easy if you are looking for the right numbers.',
     },
     {
-      id: 3,
+      id: 3, // Do not change this ever
       name: 'Sculpture',
       imageSrc: `/assets/acarolofclues/puzzleImage3.png`,
       imageAlt: '"Nick Zack"',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['1969'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'This is the sculpture on a large are of learning. What year was this constructed?',
     },
     {
-      id: 4,
+      id: 4, // Do not change this ever
       name: 'Holly Math',
       imageSrc: '/assets/acarolofclues/puzzleImage4.png',
       imageAlt: 'image of a holly branch with no berries on it, just leaves',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['644'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'The berries are your missing variable. It may all look a lot better from a new perspective.',
     },
     {
-      id: 5,
+      id: 5, // Do not change this ever
       name: 'Spine',
       imageSrc: `/assets/acarolofclues/puzzleImage5.png`,
       imageAlt: 'P1U1Z2L1E1',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['crachit'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'The sentence "This sentence" has 2 t\'s and yet you know exactly what it says.',
     },
     {
-      id: 6,
+      id: 6, // Do not change this ever
       name: 'Shape',
       imageSrc: `/assets/acarolofclues/puzzleImage6.png`,
       imageAlt: 'A Shape with 10 sides',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['&!#(+'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'These shapes have sides. These sides might be helpful in telling you the order of the symbols.',
     },
     {
-      id: 7,
+      id: 7, // Do not change this ever
       name: 'Ruler',
       imageSrc: `/assets/acarolofclues/puzzleImage7.png`,
       imageAlt:
         'Ruler with the bottom left corner cut out with a circular shape. The ruler has tick marks labeled from 0-9',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['ghost of three'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'Free the measuring device. Square it with the circles and let the number tell you the letter. Unscramble it to find your answer.',
     },
     {
-      id: 8,
+      id: 8, // Do not change this ever
       name: 'Cut Squares',
       imageSrc: '/assets/acarolofclues/puzzleImage8.png',
       imageAlt:
         'Grid of squares, 4 by 4, some squares filled in with black. Filled in coordinates using [row, column] arrangement are: [1,1], [1,4], [3,4], and [4,2]',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['09090623', '9090623', '99623'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'You may need to see throug these in order to find the right numbers',
     },
     {
-      id: 9,
+      id: 9, // Do not change this ever
       name: 'Welcome',
       imageSrc: `/assets/acarolofclues/puzzleImage9.png`,
       imageAlt:
         'Image of a black shape that looks like a horizontally stretched capital letter T',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['manhattan welcomes you'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'What does this sign say as you enter town?',
     },
     {
-      id: 10,
+      id: 10, // Do not change this ever
       name: 'Constellations',
       imageSrc: `/assets/acarolofclues/puzzleImage10.png`,
       imageAlt: 'Image of constellations',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['slats'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'The stars make a pattern, of which the first letter of the name matters. Use the moon to give you the order of those letters',
     },
     {
-      id: 11,
+      id: 11, // Do not change this ever
       name: 'Flood',
       imageSrc: `/assets/acarolofclues/puzzleImage11.png`,
       imageAlt: 'Image of a flooded area with a sign that says "Flood"',
-      isSolved: false,
+      isSolved: false, // Do not change this ever
       answers: ['1903', '1844', '1951', '1993'],
-      timeSolved: undefined,
-      tryCount: 0,
+      timeSolved: undefined, // Do not change this ever
+      tryCount: 0, // Do not change this ever
       hint: 'Any year it can rain, but one happened to be an issue for the town of Manhattan, KS.',
     },
   ],
@@ -235,6 +235,6 @@ export const faqs = [
   {
     question: 'Is there a way to get hints?',
     answer:
-      "Yes, there's a \"hint toggle\" button on the book's answer entry page. It reveals additional clues for puzzles that you've attempted more than 5 times. If you find a puzzle too difficult, you can use the hints. We don't keep track of the hint button's use, so it's on the honor system if you're competing with friends or family.",
+      "Yes, there's a \"hint change\" button on the book's answer entry page. It reveals additional clues for puzzles that you've attempted more than 5 times. If you find a puzzle too difficult, you can use the hints. We don't keep track of the hint button's use, so it's on the honor system if you're competing with friends or family.",
   },
 ];
